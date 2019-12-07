0ffed3a2807368108c7b219281e90958321d3d8a
Matthew Blode <m@blode.co>
2019-12-07 12:10:29 +1100

    File structure is working

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mblode/gonotes
+
+go 1.13
+
+require (
+	github.com/gin-contrib/static v0.0.0-20191128031702-f81c604d8ac2
+	github.com/gin-gonic/gin v1.5.0
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/yuin/goldmark v1.1.12
+	gopkg.in/russross/blackfriday.v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/yuin/goldmark"
 )
+
+// Post struct for markdown
+type Post struct {
+	Slug    string
+	Title   string
+	Content template.HTML
+}
+
+// Directory struct for markdown
+type Directory struct {
+	Slug  string
+	Title string
+}
 
 // CopyDirectory will copy the contents of a directory
 func CopyDirectory(src, dest string) error {
@@ -122,8 +144,12 @@ func check(e error) {
 }
 
 func main() {
-	var src = flag.String("src", "/Users/mblode/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/", "The folder where the files should be copied from")
-	var dest = flag.String("dest", "/Users/mblode/Google Drive/Backups/Notes", "The folder that the files should be copied to")
+	// srcDir := "/Users/mblode/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/"
+	srcDir := "/Users/mblode/Google Drive/Backups/Notes"
+	destDir := "/Users/mblode/Google Drive/Backups/Notes2"
+
+	src := flag.String("src", srcDir, "The folder where the files should be copied from")
+	dest := flag.String("dest", destDir, "The folder that the files should be copied to")
 	flag.Parse()
 
 	if err := CreateIfNotExists(*dest, os.ModePerm); err != nil {
@@ -134,4 +160,94 @@ func main() {
 	check(err)
 
 	fmt.Println("Successfully copied files")
+
+	r := gin.Default()
+	r.Use(gin.Logger())
+	r.Delims("{{", "}}")
+
+	r.Use(static.Serve("/assets", static.LocalFile("/assets", false)))
+	r.LoadHTMLGlob("./templates/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		var directories []Directory
+
+		files, err := ioutil.ReadDir(destDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			if !strings.HasPrefix(file.Name(), ".") {
+				directories = append(directories, Directory{Slug: file.Name(), Title: file.Name()})
+			}
+		}
+
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"directories": directories,
+		})
+	})
+
+	r.GET("/:dirName", func(c *gin.Context) {
+		var posts []Post
+
+		dirName := c.Param("dirName")
+
+		files, err := ioutil.ReadDir(destDir + "/" + dirName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			if !strings.HasPrefix(file.Name(), ".") {
+				posts = append(posts, Post{Slug: dirName + "/" + file.Name(), Title: file.Name()})
+			}
+		}
+
+		// if the folder can not be found
+		if err != nil {
+			fmt.Println(err)
+			c.HTML(http.StatusNotFound, "error.html", nil)
+			return
+		}
+
+		dir := Directory{Slug: dirName, Title: dirName}
+
+		c.HTML(http.StatusOK, "directory.html", gin.H{
+			"ParentSlug": dirName,
+			"Slug":       dir.Slug,
+			"Title":      dir.Title,
+			"posts":      posts,
+		})
+	})
+
+	r.GET("/:dirName/:postName", func(c *gin.Context) {
+		dirName := c.Param("dirName")
+		postName := c.Param("postName")
+
+		mdfile, err := ioutil.ReadFile(destDir + "/" + dirName + "/" + postName)
+
+		// if the file can not be found
+		if err != nil {
+			fmt.Println(err)
+			c.HTML(http.StatusNotFound, "error.html", nil)
+			return
+		}
+
+		var buf bytes.Buffer
+		if err := goldmark.Convert(mdfile, &buf); err != nil {
+			panic(err)
+		}
+
+		result := template.HTML(buf.String())
+
+		post := Post{Slug: postName, Title: postName, Content: result}
+
+		c.HTML(http.StatusOK, "post.html", gin.H{
+			"Slug":    post.Slug,
+			"Title":   post.Title,
+			"Content": post.Content,
+		})
+	})
+
+	r.Run()
 }
